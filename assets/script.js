41139712a425c5892945e033c13e3275fb5cadd7
Hazkeel Qureshi <hazkeel27@gmail.com>
2023-06-18 03:25:12 -0400

    prompts to collect criteria are added

--- assets/script.js
+++ assets/script.js
@@ -15,5 +15,41 @@ generateBtn.addEventListener("click", writePassword);
 // This function asks the user password criteria and returns a generated password of random characters
 function generatePassword()
 {
-    
+    var lengthWhileLoop = true; // this value determines when the while loop will start/end
+    while (lengthWhileLoop === true)
+    {
+        // length variable stores length of password
+        var length = window.prompt("Choose password length (between 8 to 128 characters)\nNote: Decimal values will not be considered");
+        
+        // if length does not meet criteria, password is not generated and alert message is displayed
+        if (length < 8 || length > 128)
+        {
+            // alert to show user that they have entered an invalid length
+            window.alert("Invalid password length!");
+        }
+        else
+        {
+            length = Math.floor(length); // decimal values will not be consiered
+
+            var criteriaWhileLoop = true; // this value determines when the while loop will start/end
+            while (criteriaWhileLoop === true)
+            {
+                var password = ''; // generated password variable
+                var lowercase = window.confirm("Do you want lowercase characters?"); // confirms if user wants lowercase values
+                var uppercase = window.confirm("Do you want uppercase characters?"); // confirms if user wants uppcase values
+                var number = window.confirm("Do you want numbers?"); // confirms if user wants number values
+                var special = window.confirm("Do you want special characters?"); // confirms if user wants special values
+
+                // if no criteria is chosen, user is alerted to choose atleast one criteria, else while loop ends
+                if (lowercase === false && uppercase === false && number === false && special === false)
+                {
+                window.alert("Choose atleast one criteria!");
+                }
+                else
+                {
+                criteriaWhileLoop = false;
+                }
+            }
+        }
+    }
 }
