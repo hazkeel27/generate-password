993ae4203f584ce7b1851d5238ce18c317d03991
Hazkeel Qureshi <hazkeel27@gmail.com>
2023-06-18 03:21:31 -0400

    initial code provided by instructor

---
+++ assets/script.js
@@ -0,0 +1,19 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+// This function asks the user password criteria and returns a generated password of random characters
+function generatePassword()
+{
+    
+}
